f672e937cf658a94621ab8d660d318fc659c51cf
Nguyễn Minh Phương <nguyenminhphuong813@gmail.com>
2024-03-18 14:15:13 +0700

    first commit

---
+++ src/main/java/com/codegym/minitesttuan3m4/MinitestTuan3M4Application.java
@@ -0,0 +1,13 @@
+package com.codegym.minitesttuan3m4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinitestTuan3M4Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MinitestTuan3M4Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/codegym/minitesttuan3m4/MinitestTuan3M4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codegym.minitesttuan3m4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinitestTuan3M4ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
